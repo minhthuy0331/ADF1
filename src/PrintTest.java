e29136dbeaa5b57bf30422d666d5977e106d04f4
Minh Thúy <minhthuy32003@gmail.com>
2022-04-06 14:48:39 +0700

    Task2

---
+++ src/PrintTest.java
@@ -0,0 +1,14 @@
+public class PrintTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world again!");
+        System.out.println();
+        System.out.println("Hello world!");
+        System.out.println("Hello world again!");
+        System.out.println();
+        System.out.println("Hello,");
+        System.out.println(" ");
+        System.out.println("world");
+        System.out.println("Hello, world!");
+    }
+}
